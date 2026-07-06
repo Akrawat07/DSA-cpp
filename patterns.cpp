2f9c773f1fd87e6b5cfd45a9eeacd6419ff6da24
Akshita <your-email@gmail.com>
2026-07-06 13:01:55 +0530

    did tilll 2nd pattern

---
+++ patterns.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+//pattern 1
+/*void printN(int N,int M){
+    for (int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+    
+OUTPUT:-
+*****
+*****
+*****
+*****
+*/
+
+
+
+//pattern 2
+/*void printN(int N,int M){
+    for (int i=1;i<=N;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+OUTPUT:-
+1
+12
+123
+1234*/
+
+//Pattern 3
+void printN(int rows){
+    int n=1;
+    while(n<=rows){
+        int cols=1;
+        while(cols<=rows){
+            cout<<
+        }
+
+    }
+}
+
+
+int main(){
+    
+    return 0;
+}

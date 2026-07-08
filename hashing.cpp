06861403a3edbc9637c0f3665927e14eb59b0c2e
Akshita <your-email@gmail.com>
2026-07-08 18:26:04 +0530

    Two sum Problem

--- hashing.cpp
+++ hashing.cpp
@@ -26,3 +26,17 @@ int main() {
     cout << endl;
     return 0;
 }
+
+
+/*
+Two sum problem:- find the sum of the targeted element in the vector and return their indices
+Optimal approach:-here unordered map is used to store the elements of the vector along with their indices
+example:-
+1. enter the {m[nums[i]],i }in the hash if it is not in the m(ie. map)
+2. then,find the other=target-nums[i] in the map
+it is stored like this:
+2->0
+7->1
+11->2
+15->3 
+*/

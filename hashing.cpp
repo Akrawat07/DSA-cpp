fdc66af38381a9098709031e8ebee72af96512b8
Akshita <your-email@gmail.com>
2026-07-08 09:10:40 +0530

    Two sum using hashing

--- hashing.cpp
+++ hashing.cpp
@@ -1,31 +1,28 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
 #include <unordered_map>
-
+#include <vector>
 using namespace std;
 
-vector<int> twosum(vector<int> nums,int target){
-    unordered_map<int,int> m;
-    for (int i : nums){
-        m[nums[i]]=i;
-    }
-    for(int i=0;i<m.size();i++){
-        int other=target-nums[i];
-        if(m.find(other)!=m.end() && m[other]!=i){
-            return {i,m[other]};
+vector<int> twosum(vector<int> nums, int target) {
+    unordered_map<int, int> m;
+    for (int i = 0; i < nums.size(); i++) {
+        int other = target - nums[i];
+        if (m.find(other) != m.end()) {
+            return {i, m[other]};
         }
-        
+        m[nums[i]] = i;
     }
     return {};
 }
-int main(){
-    vector<int> nums={2,7,11,15};
-    int target=9;
-    vector<int> result=twosum(nums,target);
-    for(int i:result){
-        cout<<i<<" ";
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = twosum(nums, target);
+
+    for (int i : result) {
+        cout << i << " ";
     }
+    cout << endl;
     return 0;
 }
-
-    

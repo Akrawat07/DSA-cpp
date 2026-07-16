13a38b43513335fb4799e4d16d8aa3e3ad7fc40e
Akshita <your-email@gmail.com>
2026-07-17 01:20:54 +0530

    palindrome using recursion

--- Recursion.cpp
+++ Recursion.cpp
@@ -11,7 +11,7 @@ using namespace std;
 gives o/p:
 5 4 3 2 1 */
 
-void palindrome(int i,string str,int n){
+/*void palindrome(int i,string str,int n){
     if (i>=n/2)return ;
     if(str[i]==str[n-i-1]){
         cout<<"yes"<<endl;
@@ -20,6 +20,15 @@ void palindrome(int i,string str,int n){
     else cout<<"no"<<endl;
 
 }
+*/
+// using recursion
+
+bool palindrome (string str,int i,int n){
+    if (i>=n/2) return true; //if we exceeds  middle of the string, it means all characters matched and is palindrome 
+    if(str[i]!=str[n-i-1]) return false;// if any character does not match, it is not a palindrome
+    return palindrome(str,i+1,n); //recursive call to check the next characters
+}
+
 int main(){
 
     string str;
@@ -28,7 +37,7 @@ int main(){
     int n=str.length();
     
     cout<<"palindrome or not: ";
-    palindrome(0,str,n);
+    cout<< palindrome(str,0,n);
     return 0;
 }
 

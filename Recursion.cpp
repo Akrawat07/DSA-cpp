43775072d7141f4760b3aaeb1361d4b078c9af61
Akshita <your-email@gmail.com>
2026-07-16 17:31:23 +0530

    palindrome no .

--- Recursion.cpp
+++ Recursion.cpp
@@ -2,21 +2,33 @@
 #include<vector>
 #include<utility>
 using namespace std;
-void reverse(int i ,int arr[],int n){
+/*void reverse(int i ,int arr[],int n){
     if(i>=n/2) return ;
     swap(arr[i],arr[n-i-1]);
     reverse(i+1,arr,n);
     
 }
-/*
 gives o/p:
 5 4 3 2 1 */
+
+void palindrome(int i,string str,int n){
+    if (i>=n/2)return ;
+    if(str[i]==str[n-i-1]){
+        cout<<"yes"<<endl;
+        return ;
+    }
+    else cout<<"no"<<endl;
+
+}
 int main(){
-    int arr[]={1,2,3,4,5};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    reverse(0,arr,n);
-    cout<<"Reversed array is : ";
-    for(int i=0;i<n;i++) cout<<arr[i]<<" ";
+
+    string str;
+    cout<<"Enter the string:";
+    cin>>str;
+    int n=str.length();
+    
+    cout<<"palindrome or not: ";
+    palindrome(0,str,n);
     return 0;
 }
 
